d03acc8b6c048c36d9794708ef5b9393ad53b020
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-12 15:06:00 -0600

    refactor: update remove method in PokemonService to accept string id and handle deletion

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -75,8 +75,9 @@ export class PokemonService {
     };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    const pokemon = await this.findOne(id);
+    await pokemon.deleteOne();
   }
 
   private handleExceptions(error: Error) {
